c38ed26aa390840e02a2d5dccccb88f59e734b81
xcanndy <ola.chmur@gmail.com>
2018-06-19 18:42:19 +0200

    - animation for marker - remove controls from map (zoom, street view)

--- app/js/initMap.js
+++ app/js/initMap.js
@@ -1,21 +1,15 @@
 window.addEventListener('load', () => initMap)
 
 function initMap() {
-  // Basic options for a simple Google Map
-  // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+
   var mapOptions = {
-    // How zoomed in you want the map to start at (always required)
     zoom: 4,
-    // MouseEvent: google.maps.stop,
     fullscreenControl: false,
     mapTypeControl: false,
     minZoom: 4,
-
-    // The latitude and longitude to center the map (always required)
-    center: new google.maps.LatLng(52.387733, 18.4272), // New York
-
-    // How you would like to style the map. 
-    // This is where you would paste any style found on Snazzy Maps.
+    zoomControl: false,
+    streetViewControl: false,
+    center: new google.maps.LatLng(53.1159665, 23.1276987), 
     styles: [{
         "featureType": "all",
         "elementType": "labels.text.fill",
@@ -208,18 +202,28 @@ function initMap() {
     ]
   };
 
-  // Get the HTML DOM element that will contain your map 
-  // We are using a div with id=”map” seen below in the <body>
   var mapElement = document.getElementById('map');
 
-  // Create the Google Map using our element and options defined above
   var map = new google.maps.Map(mapElement, mapOptions);
 
-  // Let’s also add a marker while we’re at it
   var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(53.115837, 23.127268),
+    position: new google.maps.LatLng(53.1158368, 23.1272678),
     map: map,
     title: 'Infinity Group Sp. z o.o.',
-    icon: './images/pin.png'
+    icon: './images/pin.png',
+    animation: google.maps.Animation.DROP,
+  });
+
+  map.addListener('center_changed', function() {
+    // 3 seconds after the center of the map has changed, pan back to the
+    // marker.
+    window.setTimeout(function() {
+      map.panTo(marker.getPosition());
+    }, 3000);
+  });
+
+  marker.addListener('click', function() {
+    map.setZoom(16);
+    map.setCenter(marker.getPosition());
   });
 }
