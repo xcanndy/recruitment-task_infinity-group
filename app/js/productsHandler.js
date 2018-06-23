5c6eaaa19b50ddbc5641340fd362e166623533d4
xcanndy <ola.chmur@gmail.com>
2018-06-23 22:08:42 +0200

    v.1.0.5

--- app/js/productsHandler.js
+++ app/js/productsHandler.js
@@ -16,7 +16,7 @@ const createProductHtml = product => {
   return productContainer;
 }
 
-fetch('/products.json')
+fetch('./products.json')
   .then(response => {
     return response.json()
       .then(data => {
